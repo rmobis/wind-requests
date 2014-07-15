495437bf9bee32ffd1f85ec1d4ee48296a54ee52
rmobis <r.mobis@gmail.com>
2014-07-15 10:05:35 -0300

    Fix Loot Logger Shit was completely fucked up, logging all kinds of stupid message and shit

--- Hotkeys/Loot Logger.lua
+++ Hotkeys/Loot Logger.lua
@@ -1,5 +1,5 @@
 init start
-	-- local SCRIPT_VERSION = '1.0.1'
+	-- local SCRIPT_VERSION = '1.1.0'
 
 	local filename = 'Loot - ' .. $name .. '.txt'
 	local hideEmpty = false
@@ -11,12 +11,16 @@ auto(1000)
 
 local handler = nil
 foreach newmessage m do
-	if m.type == MSG_INFO and not (hideEmpty and m.content:find('nothing')) then
-		if handler == nil then
-			handler = io.open(filename, 'a+')
-		end
+	if m.type == MSG_INFO then
+		local _, loot = m.content:match(REGEX_LOOT)
 
-		handler:write(os.date('%H:%M') .. ' ' .. m.content .. '\n')
+		if loot and (not hideEmpty or loot ~= 'nothing') then
+			if handler == nil then
+				handler = io.open(filename, 'a+')
+			end
+
+			handler:write(os.date('%H:%M') .. ' ' .. m.content .. '\n')
+		end
 	end
 end
 
