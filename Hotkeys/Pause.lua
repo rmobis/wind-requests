702ab14a87a8c2eaf4843a6c631fc1dd3ee7fb08
rmobis <r.mobis@gmail.com>
2014-07-24 19:13:38 -0300

    Add Pause

---
+++ Hotkeys/Pause.lua
@@ -0,0 +1,28 @@
+init start
+	-- local SCRIPT_VERSION = '1.0.0'
+
+	-- What settings should be paused/unpaused
+	local settingsToggable = {
+		Cavebot = true,
+		Looting = true,
+		Targeting = true,
+		SpellHealer = false,
+		PotionHealer = false,
+		ConditionHealer = false,
+		ManaTrainer = false
+	}
+
+	-- DO NOT EDIT BELOW THIS LINE --
+init end
+
+local curState = false
+for k, v in pairs(settingsToggable) do
+	curState = curState or (v and tobool(get(k .. '/Enabled')))
+end
+
+local newState = toyesno(not curState)
+for k, v in pairs(settingsToggable) do
+	if v then
+		set(k .. '/Enabled', newState)
+	end
+end
