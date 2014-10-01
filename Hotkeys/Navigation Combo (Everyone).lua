9c53d311b1c8cad094d8182ab9a11d05bea2ecc9
Raphael Mobis Tacla <r.mobis@gmail.com>
2014-10-01 17:05:20 -0300

    Navigation SD Combo (Everyone): Fix issue with non-command messages

--- Hotkeys/Navigation Combo (Everyone).lua
+++ Hotkeys/Navigation Combo (Everyone).lua
@@ -1,5 +1,5 @@
 init start
-	-- local SCRIPT_VERSION = '1.1.1'
+	-- local SCRIPT_VERSION = '1.1.2'
 
 	local commands = {
 		sd = {
@@ -14,11 +14,12 @@ init start
 	function navmessages(m)
 		local comm, args = m.message:match('(.-):(.+)')
 		comm = commands[comm]
-		args = args:explode(';')
-		table.map(args, function(arg) return string.trim(arg) end)
 
 		if comm then
-			if not comm.leaderOnly or m.isleader then
+			args = args:explode(';')
+			table.map(args, function(arg) return string.trim(arg) end)
+	
+			if m.isleader or not comm.leaderOnly then
 				comm.action(table.unpack(args))
 			end
 		end
