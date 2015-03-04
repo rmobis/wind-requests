fb3032633769f95a642b653653ee58458b0e2507
Raphael Mobis Tacla <r.mobis@gmail.com>
2015-03-04 19:04:26 -0300

    Add minimum damage option to Anti Stalker

--- Hotkeys/Anti Stalker.lua
+++ Hotkeys/Anti Stalker.lua
@@ -1,33 +1,34 @@
 init start
-	-- local SCRIPT_VERSION = '1.0.1'
+    -- local SCRIPT_VERSION = '1.1.0'
 
-	local invisCreatures = {'Stalker'}
-	local stuckTime = 0 -- Set to 0 to always attack
-	local spellToUse = 'exori'
+    local invisCreatures = {'Stalker'}
+    local stuckTime = 0 -- Set to 0 to always attack
+    local minDamageTaken = 0 -- Set to 0 to always attack
+    local spellToUse = 'exori'
 
-	-- DO NOT EDIT BELOW THIS LINE --
-	table.lower(invisCreatures)
-	local spellToUseInfo, spellType = spellinfo(spellToUse), 'spell'
-	if spellToUseInfo.castarea == 'None' then
-		spellToUseInfo, spellType = runeinfo(spellToUse), 'rune'
-	end
+    -- DO NOT EDIT BELOW THIS LINE --
+    table.lower(invisCreatures)
+    local spellToUseInfo, spellType = spellinfo(spellToUse), 'spell'
+    if spellToUseInfo.castarea == 'None' then
+        spellToUseInfo, spellType = runeinfo(spellToUse), 'rune'
+    end
 init end
 
 auto(100)
 
 if $standtime >= stuckTime then
-	foreach newmessage m do
-		if m.type == MSG_STATUSLOG then
-			local _, _, name = m.content:match(REGEX_DMG_TAKEN)
-			if name and table.find(invisCreatures, name:lower()) and maround(7, name:lower()) == 0 then
-				if spellType == 'spell' and cancastspell(spellToUseInfo) then
-					cast(spellToUseInfo.words)
-					waitping()
-				elseif spellType == 'rune' then
-					useoncreature(spellToUseInfo.itemid, $self)
-					waitping()
-				end
-			end
-		end
-	end
+    foreach newmessage m do
+        if m.type == MSG_STATUSLOG then
+            local dmg, _, name = m.content:match(REGEX_DMG_TAKEN)
+            if dmg >= minDmgTaken and name and table.find(invisCreatures, name:lower()) and maround(7, name:lower()) == 0 then
+                if spellType == 'spell' and cancastspell(spellToUseInfo) then
+                    cast(spellToUseInfo.words)
+                    waitping()
+                elseif spellType == 'rune' then
+                    useoncreature(spellToUseInfo.itemid, $self)
+                    waitping()
+                end
+            end
+        end
+    end
 end
