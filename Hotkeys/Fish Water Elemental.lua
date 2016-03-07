bf74e12ab5b81e9f78b1ffc57732476d241b4886
mistgun <nikodem.pierzak@gmail.com>
2016-03-07 19:21:30 +0100

    Fixed stuck on non-shootable body, added new body id, removed wait fresh option, added optional checks for rod hotkey

--- Hotkeys/Fish Water Elemental.lua
+++ Hotkeys/Fish Water Elemental.lua
@@ -1,61 +1,46 @@
 init start
-	-- local SCRIPT_VERSION = '1.1.1'
-
-	-- If set to true, will pause walking and wait for fresh bodies to become
-	-- 'fisable'.
-	local waitFresh = false
-
+	-- local SCRIPT_VERSION = '1.1.2'
 
 	-- DO NOT EDIT BELOW THIS LINE --
-
 	local fishLoot = {281, 282, 3026, 3029, 3032, 9303}
 
-	-- I wish I could have made this a more generic function, but it would most
-	-- likely make it less efficient for the current use case; I know... I will
-	-- pay for that in the future. Meh... whatever, bring it on!
 	local function findwaterfishspots(exceptClose)
-		local hasFresh = false
-
 		for x = -7, 7 do
 			for y = -5, 5 do
-				-- This lets us ignore bodies that are too close and might fuck
-				-- up because of character's first movements, but still account
-				-- for them after it's already standing.
 				if not exceptClose or (math.abs(x) > 1 or math.abs(y) > 1) then
 					local curX, curY = $posx + x, $posy + y
 					local item = topuseonitem(curX, curY, $posz).id
 
-					if item == 9582 then
+					if table.find({9582, 4037}, item) then
 						return {x = curX, y = curY}
-					elseif item == 4037 then
-						hasFresh = true
 					end
 				end
 			end
 		end
 
-		return nil, hasFresh
+		return nil
 	end
 init end
 
 auto(100)
 
-if $lootsaround == 0 and $targetingtarget.hppc == 0 then
-	local old, fresh = findwaterfishspots(true)
+if $lootsaround == 0 and $targetingtarget.hppc == 0 and (clientitemhotkey(3483, 'crosshairs') ~= 'not found' or $fasthotkeys) then
+	local body = findwaterfishspots(true)
 
-	while old ~= nil or (waitFresh and fresh) do
+	while body ~= nil and tileshootable(body.x, body.y, $posz) do
+
 		pausewalking(6^9) -- Yeah babe!
 
-		if old ~= nil then
+		if body ~= nil then
 			local itemCount = {}
 
 			for _, v in ipairs(fishLoot) do
 				itemCount[v] = itemcount(v)
 			end
 
-			useitemon(3483, 0, ground(old.x, old.y, $posz))
+			useitemon(3483, 0, ground(body.x, body.y, $posz))
 			waitping()
-
+			
 			for _, v in ipairs(fishLoot) do
 				local curCount = itemcount(v)
 
@@ -70,7 +55,7 @@ if $lootsaround == 0 and $targetingtarget.hppc == 0 then
 			break
 		end
 
-		old, fresh = findwaterfishspots(false)
+		body = findwaterfishspots(false)
 	end
 
 	pausewalking(0) -- Orgasm.
